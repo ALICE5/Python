ab1856a1442eab03e9c9f21219b5c5fe5657f404
Yan <alice4u@bjtu.edu.cn>
2017-07-23 23:59:18 +0800

    Pray

--- py_prac/生成激活码/prac_2.py
+++ py_prac/生成激活码/prac_2.py
@@ -12,39 +12,18 @@ random_codes = lambda x,y : ''.join([random.choice(x) for i in range(y)])
 print(random_codes(poolOfChar,15))
 
 # 填充空位
+# 主键通常是一个递增的整数序列 若主键值从1增加到1000 那么主键的位数就会从1增加到4
+# 对于邀请码来说需要固定其长度 反应到主键上需要固定主键的长度
+# 最简单的办法就是用 0 填充（pad）空位
 class LengthError(ValueError):
-   def __init__(self, arg):
-      self.args = arg
+    def __init__(self,arg):
+        self.args = arg
 
 def pad_zero_to_left(inputNumString, totalLength):
-    '''
-    takes inputNumString as input,
-    pads zero to its left, and make it has the length totalLength
-    1. calculates the length of inputNumString
-    2. compares the length and totalLength
-        2.1 if length > totalLength, raise an error
-        2.2 if length == totalLength, return directly
-        2.3 if length < totalLength, pads zeros to its left
-    '''
     lengthOfInput = len(inputNumString)
     if lengthOfInput > totalLength:
-        raise LengthError("The length of input is greater than the total\ length.")
+        raise LengthError("The length of input is greater than the total length.")
     else:
         return '0' * (totalLength - lengthOfInput) + inputNumString
 
 # 拼接
-def invitation_code_generator(quantity, lengthOfRandom, LengthOfKey):
-    '''
-    generate `quantity` invitation codes
-    '''
-    placeHoldChar = "L"
-    for index in range(quantity):
-        tempString = ""
-        try:
-            yield random_codes(poolOfChar, lengthOfRandom) + placeHoldChar + \
-                pad_zero_to_left(str(index), LengthOfKey)
-        except LengthError:
-            print("Index exceeds the length of master key.")
-
-for invitationCode in invitation_code_generator(200, 15, 4):
-    print(invitationCode)
