6ebb2eb45e5b244f1fe5e817152f22b40c52fe2d
Yan <alice4u@bjtu.edu.cn>
2017-07-28 19:32:12 +0800

    prac_9

---
+++ py_prac/找出HTML链接/prac_9.py
@@ -0,0 +1,20 @@
+# 一个HTML文件 找出里面的链接
+
+#! /usr/bin/env/ python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Alice'
+
+import urllib.request
+from bs4 import BeautifulSoup
+
+url = "https://www.liaoxuefeng.com/wiki/0014316089557264a6b348958f449949df42a6d3a2e542c000"
+page = urllib.request.urlopen(url)
+
+soup = BeautifulSoup(page,'html.parser')
+links = soup.find_all('a')
+# links = soup.select('a')
+
+for link in links:
+    print(link['href'])
+    # print(link)
