cc064dd22bb54600f75b77eea41751ddbc0fba53
Yan <alice4u@bjtu.edu.cn>
2017-07-31 23:57:07 +0800

    prac_10

--- py_prac/字母验证码/prac_10.py
+++ py_prac/字母验证码/prac_10.py
@@ -1,35 +1,13 @@
 #-*- coding: utf-8 -*-
 
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
-import string
 import random
 
+def rndChar():
+    return chr(random.randint(65,90))
 
-def get4char():
-    return [random.choice(string.ascii_letters) for _ in range(4)]  # 可以把‘_’换做任意字母，‘_’说明后续不用
+def rndColor1():
+    return ()
 
-
-def getColor():
-    return random.randint(30, 100), random.randint(30, 100), random.randint(30, 100)
-
-
-def getPicture():
-    width = 240
-    height = 60
-
-    image = Image.new('RGB', (width, height), (180, 180, 180))
-    font = ImageFont.truetype('arial.ttf', 40)
-
-    draw = ImageDraw.Draw(image)
-    code = get4char()
-    for ch in range(4):
-        draw.text((60 * ch,30), code[ch], font=font, fill=getColor())
-
-    for _ in range(random.randint(1500, 3000)):
-        draw.point((random.randint(0, width), random.randint(0, height)), fill=getColor())
-
-    image = image.filter(ImageFilter.BLUR)
-    image.save("".join(code) + '.jpg')
-
-
-getPicture()
+def rndColor2():
+    return ()
