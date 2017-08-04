2d1f71b41b58fa97f53a7260480dfcc795f97281
Yan <alice4u@bjtu.edu.cn>
2017-08-04 23:51:00 +0800

    prac_16

--- py_prac/Number文本文件写入XLS/prac_16.py
+++ py_prac/Number文本文件写入XLS/prac_16.py
@@ -7,3 +7,13 @@
 # ]
 # 请将上述内容写到 numbers.xls 文件中
 
+import xlwt,json
+
+with open('source/0015/city.txt','r') as f:
+    data = json.load(f)
+    workbook = xlwt.Workbook()
+    sheet1 = workbook.add_sheet('city', cell_overwrite_ok=True)
+    for index, (key, value) in enumerate(data.items()):
+        sheet1.write(index, 0, key)
+        sheet1.write(index, 1, value)
+    workbook.save('source/0015/city.xls')
