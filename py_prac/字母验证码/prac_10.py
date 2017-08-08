484f460bda51061ac07deeee5e5ee832caf6925d
Yan <alice4u@bjtu.edu.cn>
2017-08-08 23:35:20 +0800

    prac_10

--- py_prac/字母验证码/prac_10.py
+++ py_prac/字母验证码/prac_10.py
@@ -1,13 +1,36 @@
 #-*- coding: utf-8 -*-
 
+#生成字母验证码图片
 from PIL import Image, ImageDraw, ImageFont, ImageFilter
 import random
 
+#随机字母(26个大写)
 def rndChar():
     return chr(random.randint(65,90))
 
-def rndColor1():
-    return ()
+#随机颜色1
+def rndColor():
+    return(random.randint(64,255),random.randint(64,255),random.randint(64,255))
 
+#随机颜色2
 def rndColor2():
-    return ()
+    return(random.randint(32,127),random.randint(32,127),random.randint(32,127))
+
+#240 x 60
+width = 60 * 4
+height = 60
+image = Image.new('RGB',(width,height),(255,255,255))
+# 创建Font对象
+font = ImageFont.truetype('/System/Library/Fonts/MarkerFelt.ttc',40)
+#创建Draw对象
+draw = ImageDraw.Draw(image)
+#填充每个像素
+for x in range(width):
+    for y in range(height):
+        draw.point((x,y),fill=rndColor())
+#输出文字
+for t in range(4):
+    draw.text((60 * t + 10, 10),rndChar(),font=font,fill=rndColor2())
+#模糊
+image = image.filter(ImageFilter.BLUR)
+image.save('code.jpg','jpeg')
