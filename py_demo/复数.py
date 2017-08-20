f1f338f7c06de62f9816557b59a7323f060e58bf
Yan <alice4u@bjtu.edu.cn>
2017-08-20 17:25:32 +0800

    demo3

---
+++ py_demo/复数.py
@@ -0,0 +1,12 @@
+PI = 3.14159
+myString = 'is a mathematic circular constant'
+print(PI,myString)
+
+t = (4+5j)*(4-5j)
+print(t)
+
+print(type(4+5j))
+
+x = 4+5j
+print(x.real,x.imag,x.conjugate())
+
---
+++ py_demo/输入输出.py
@@ -0,0 +1,11 @@
+print('Hello, World!')
+x = int(input('Enter a number:'))
+print(x+x)
+print('''
+hi everybody,welcome to \
+python's MOOC course.
+Here we an learn something about
+python.Good luck.
+''')
+# 续航符: \
+
